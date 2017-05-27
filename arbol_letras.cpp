0b0f907c1843817325dbf8d1415dad7f1bac8d48
tatanmejia <jhotan_22m@hotmail.com>
2017-05-27 15:50:56 -0500

    arbol binario Letras finalizado

--- arbol_letras.cpp
+++ arbol_letras.cpp
@@ -1,118 +1,183 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <string.h>
 
-
-struct nodoarbol{
-struct nodoarbol *izqnodo;
-int info;
-char letra;
-struct nodoarbol *dernodo;
+struct nodo{
+    char dato;
+    nodo *izquierdo;
+    nodo *derecho;
 };
-typedef struct nodoarbol NODO;
-typedef NODO *ARBOL;
 
-void insertanodonuevo(ARBOL *, int,char);
-void inorden (ARBOL);
-void preorden (ARBOL);
-void postorden (ARBOL);
-void treefree (ARBOL);
+void menu(nodo *&);
+void insertar_nodo(nodo *&,char);
+bool buscar_nodo(nodo *,char);
+void mostrar_preorden(nodo *);
+void mostrar_inorden(nodo *);
+void mostrar_posorden(nodo *);
+void modificar_nodo(nodo *&,char);
 
-main()
-{
-int i, n, elementos;
-char newnod, chain;
-ARBOL raiz=NULL;
-
-printf("\n\nCuantos nodos desea agregar: ");
-scanf("%d",&n);
-for (i=0;i<n;i++)
-{
-fflush(stdin);
-printf("\nIntroduce numero: ");
-scanf("%d",&elementos);
-fflush(stdin);
-printf("Introduce letra: ");
-scanf("%c",&chain);
-insertanodonuevo(&raiz,elementos,chain);
-}
-system("cls");
-printf("\n\nPreorden: ");
-preorden(raiz);
-printf("\n\nInorden: ");
-inorden(raiz);
-printf("\n\nPostOrden: ");
-postorden(raiz);
-getch();
-treefree(raiz);
-raiz=NULL;
-return 0;
+int main(){
+    nodo *arbol=NULL;
+    menu(arbol);
 }
 
+void menu(nodo *&arbol){
+    int j, y;
+    char a;
 
-void insertanodonuevo(ARBOL *rarbol, int nuevo,char l)
-{
-if (*rarbol==NULL)
-{
-*rarbol=(NODO *)malloc(sizeof(NODO));
-if(*rarbol!=NULL)
-{
-(*rarbol)->info=nuevo;
-(*rarbol)->letra=l;
-(*rarbol)->izqnodo=NULL;
-(*rarbol)->dernodo=NULL;
-}
-else {printf("\nMEMORIA NO DISPONIBLE\n");}
-}
-else
-if(nuevo<(*rarbol)->info)
-insertanodonuevo(&((*rarbol)->izqnodo),nuevo,l);
-else
-if(nuevo>(*rarbol)->info)
-insertanodonuevo(&((*rarbol)->dernodo),nuevo,l);
-}
-void preorden (ARBOL rarbol)
-{
-if(rarbol!=NULL)
-{
+    system("cls");
+    printf("******ARBOLES BINARIOS LETRAS*********\n");
+    printf("1. Insertar elementos\n");
+    printf("2. Mostrar arbol\n");
+    printf("3. Buscar nodo\n");
+    printf("4. Modificar nodo\n");
+    printf("5. Salir\n");
+    printf("Digite opcion: ");
+    scanf("%d",&j);
 
-printf("%d",rarbol->info);
-printf("%c",rarbol->letra);
-preorden(rarbol->izqnodo);
-preorden(rarbol->dernodo);
-}
+    switch(j){
+        case 1:{
+            do{
+               printf("Ingrese caracter: ");
+               scanf("%s",&a);
+               insertar_nodo(arbol,a);
+               printf("Ingresar mas caracteres 1.SI  2.NO: ");
+               scanf("%d",&y);
+            }while(y==1);
+        }break;
+        case 2:{
+            system("cls");
+            printf("1. Mostrar arbol en preorden\n");
+            printf("2. Mostrar arbol en inorden\n");
+            printf("3. Mostrar arbol en posorden\n");
+            printf("Digite opcion: ");
+            scanf("%d",&a);
+            switch(a){
+                case 1:{
+                    mostrar_preorden(arbol);
+                    printf("\n");
+                }break;
+                case 2:{
+                    mostrar_inorden(arbol);
+                    printf("\n");
+                }break;
+                case 3:{
+                    mostrar_posorden(arbol);
+                    printf("\n");
+                }break;
+                default: printf("Opcion incorrecta\n");
+            }
+        }break;
+        case 3:{
+            printf("Ingrese dato que desea buscar: ");
+            scanf("%s",&a);
+            if(buscar_nodo(arbol,a)==true){
+                printf("Caracter encontrado\n");
+            }else{
+                printf("Caracter no encontrado\n");
+            }
+        }break;
+        case 4:{
+            printf("Ingrese dato que desea modificar: ");
+            scanf("%s",&a);
+            modificar_nodo(arbol,a);
+        }break;
+        case 5:{
+            exit(5);
+        }
+    }
+    system("pause");
+    menu(arbol);
 }
 
-void inorden(ARBOL rarbol)
-{
-if(rarbol!=NULL)
-{
-inorden(rarbol->izqnodo);
-printf("%d", rarbol->info);
-printf("%c",rarbol->letra);
-inorden(rarbol->dernodo);
-}
+nodo *crear_nodo(char a){
+    nodo *nuevo=(nodo*)malloc(sizeof(nodo));
+    nuevo->dato=a;
+    nuevo->izquierdo=NULL;
+    nuevo->derecho=NULL;
+
+    return nuevo;
 }
 
-void postorden(ARBOL rarbol)
-{
-if(rarbol!=NULL)
-{
+void insertar_nodo(nodo *&arbol, char a){
+    int x=a;
+    if(arbol==NULL){
+        nodo *nuevo=crear_nodo(a);
+        arbol=nuevo;
+    }else{
+        int y=arbol->dato;
+        if(x<y){
+            insertar_nodo(arbol->izquierdo,a);
+        }else{
+            insertar_nodo(arbol->derecho,a);
+        }
+    }
+}
 
-postorden(rarbol->izqnodo);
-postorden(rarbol->dernodo);
-printf("%d", rarbol->info);
-printf("%c",rarbol->letra);
-}
+bool buscar_nodo(nodo *arbol, char a){
+    int x=a;
+    if(arbol==NULL){
+        return false;
+    }else{
+        if(arbol->dato==a){
+            return true;
+        }else{
+            int y=arbol->dato;
+            if(x<y){
+                return buscar_nodo(arbol->izquierdo,a);
+            }else{
+                return buscar_nodo(arbol->derecho,a);
+            }
+        }
+    }
 }
 
-void treefree(ARBOL rarbol)
-{
-if(rarbol!=NULL)
-{
-treefree (rarbol->izqnodo);
-treefree(rarbol->dernodo);
-treefree (rarbol);
+void mostrar_preorden(nodo *arbol){
+    if(arbol==NULL){
+        return;
+    }else{
+        printf("%c",arbol->dato);
+        mostrar_preorden(arbol->izquierdo);
+        mostrar_preorden(arbol->derecho);
+    }
 }
+
+void mostrar_inorden(nodo *arbol){
+    if(arbol==NULL){
+        return;
+    }else{
+        mostrar_preorden(arbol->izquierdo);
+        printf("%c",arbol->dato);
+        mostrar_preorden(arbol->derecho);
+    }
+}
+
+void mostrar_posorden(nodo *arbol){
+    if(arbol==NULL){
+        return;
+    }else{
+        mostrar_preorden(arbol->izquierdo);
+        mostrar_preorden(arbol->derecho);
+        printf("%c",arbol->dato);
+    }
+}
+
+void modificar_nodo(nodo *&arbol, char a){
+    int x=a;
+    char c;
+    if(arbol==NULL){
+        return;
+    }else{
+        if(arbol->dato==a){
+        printf("Ingrese nuevo dato: ");
+        scanf("%s",&c);
+        arbol->dato=c;
+        }else{
+            int y=arbol->dato;
+            if(x<y){
+                modificar_nodo(arbol->izquierdo,a);
+            }else{
+                modificar_nodo(arbol->derecho,a);
+            }
+        }
+    }
 }
